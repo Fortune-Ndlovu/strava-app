10a6812c898f1f851237619936721fcdeda9b078
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-08-02 17:58:12 +0100

    We are starting to create our global search input. This commit contains bugs because we have not yet created our handles.

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,11 +11,24 @@ const Header = () => {
             <Navbar expand="lg" className="bg-body-tertiary">
                 <Container>
                     <Navbar.Brand href="#home">
-                        <img src={stravaLogo} alt='company brand logo that is the word strava.'/>
+                        <img src={stravaLogo} alt='Company brand logo that simply says strava.'/>
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
+                        <Nav className="me-auto">
+                            {/* Search functionality */}
+                            <form>
+                                <button id='open-global-search-btn'>Open Search</button>
+                                <div>
+                                    <input typeof='text' id='global-search-input'><button id='global-search-btn'>Search</button></input>
+                                    <button id='close-global-search-btn'>Close search</button>
+
+                                    <input type='select'>
+                                        <option>Athletes</option>
+                                        <option>Clubs</option>
+                                    </input>
+                                </div>
+                            </form>
                         <Nav.Link href="#home">Dashboard</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
