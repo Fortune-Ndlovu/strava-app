6293dadded2030e6935b835f079524d69843008d
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-08-09 20:32:24 +0100

    React Icons library installed

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import stravaLogo from '../images/strava_logo.svg';
+import { FiSearch } from 'react-icons/fi';
 
 const Header = () => {
     return (
@@ -16,19 +17,7 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            {/* Search functionality */}
-                            <form>
-                                {/* <button id='open-global-search-btn'>Open Search</button>
-                                <div>
-                                    <input typeof='text' id='global-search-input'><button id='global-search-btn'>Search</button></input>
-                                    <button id='close-global-search-btn'>Close search</button>
-
-                                    <input type='select'>
-                                        <option>Athletes</option>
-                                        <option>Clubs</option>
-                                    </input>
-                                </div> */}
-                            </form>
+                        <Nav.Link href="#home"><FiSearch className='open-search-icon'/></Nav.Link>
                         <Nav.Link href="#home">Dashboard</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
