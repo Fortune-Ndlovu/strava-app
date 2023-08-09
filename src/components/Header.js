e77a472dd823ce73cfd08727314aa57e0fa95418
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-08-09 23:20:52 +0100

    Adjusting our NavDropdown component to have clean usability, we are starting with the first dropdown once its smoother the following NavDropdowns will follow

--- src/components/Header.js
+++ src/components/Header.js
@@ -17,8 +17,10 @@ const Header = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                        <Nav.Link href="#home"><FiSearch className='open-search-icon'/></Nav.Link>
+                        
+                        <Nav.Link href="#home"><FiSearch className='open-search-icon' /></Nav.Link>
                         <Nav.Link href="#home">Dashboard</Nav.Link>
+                            
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
