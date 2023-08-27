e75ccb1ee0a8d5933d53ad97ae151f7de3188df4
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-08-27 12:15:25 +0100

    the SearchBar component is a reusable piece of UI that provides a search input field and a "Cancel" button. It uses the useState hook to manage the search query state and provides callback functions (onCancel) for handling cancel actions.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,28 @@
 import React, { useState } from 'react';
 
-const SearchBar = () => {
+// functional component that takes a single prop called onCancel
+const SearchBar = ({ onCancel }) => {
+
+    // creating a piece of state with its initial value being an empty string
+    const [searchQuery, setSearchQuery] = useState("");
+
+    // Called whenever the value of the input field changes
+    // using setSearchQuery function to update the searchQuery state with the new value of the input field
+    const handleSearchChange = (event) => { 
+        setSearchQuery(event.target.value);
+    }
+
+    // Called whenever the Cancel button is clicked
+    // It clears the searchQuery state by setting it to empty string and then calls the onCancel function that was passed as a prop
+    const handleCancel = () => { 
+        setSearchQuery("");
+        onCancel();
+    }
 
     return (
-        <div>
-
+        <div className="search-bar">
+            <input type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
+            <button onClick={handleCancel}>Cancel</button>
         </div>
     )
 
