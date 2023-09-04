ef8635190758f8984822eb84a2f6d04d5a2722ec
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-09-04 19:14:01 +0100

    We added a new piece of state selectedOption to track the selected search option.
    The handleOptionChange function is called whenever the user selects an option from the dropdown, and it updates the selectedOption state.
    
    Inside the JSX, we use Bootstrap's input-group to create the desired layout. It includes the select dropdown, the search input, and the "S" button.
    
    The select dropdown has a placeholder option for the user to select an option. You can add more option elements with different search criteria as needed.
    
    We applied Bootstrap classes to style the components, such as form-select and form-control for the select and input elements, respectively. The buttons also have Bootstrap styles for primary and danger buttons.
    
    The "S" button is used for triggering the search action. You can customize its appearance and functionality as needed.
    
    The "X" button, as before, is used to cancel the search and clear the input field.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,12 +5,18 @@ const SearchBar = ({ onCancel }) => {
 
     // creating a piece of state with its initial value being an empty string
     const [searchQuery, setSearchQuery] = useState("");
+    const [selectedOption, setSelectedOption] = useState(""); // New state for the selected option
 
     // Called whenever the value of the input field changes
     // using setSearchQuery function to update the searchQuery state with the new value of the input field
     const handleSearchChange = (event) => { 
         setSearchQuery(event.target.value);
     }
+
+    const handleOptionChange = (event) => {
+        setSelectedOption(event.target.value);
+    }
+
 
     // Called whenever the Cancel button is clicked
     // It clears the searchQuery state by setting it to empty string and then calls the onCancel function that was passed as a prop
@@ -20,9 +26,20 @@ const SearchBar = ({ onCancel }) => {
     }
 
     return (
-        <div className="search-bar">
-            <input type="text" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
-            <button onClick={handleCancel}>Cancel</button>
+         <div className="search-bar">
+            <div className="input-group">
+                {/* Select dropdown for search options */}
+                <select className="form-select" value={selectedOption} onChange={handleOptionChange}>
+                    <option value="A">A </option>
+                    <option value="B">B </option>
+                    <option value="C">C </option>
+                    {/* Add more option elements here */}
+                </select>
+                {/* Search input */}
+                <input type="text" className="form-control" placeholder="Search..." value={searchQuery} onChange={handleSearchChange} />
+                <button type="button" className="btn btn-primary">S</button>
+            </div>
+            <button onClick={handleCancel} className="btn btn-danger">X</button>
         </div>
     )
 }
