a8a03fbfd2009864b83f995a5ae333a27d355173
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-09-10 13:28:03 +0100

    We are now retrieving and displaying both our activities and profile data

--- src/App.js
+++ src/App.js
@@ -4,53 +4,79 @@ import Header from './components/Header';
 function App() {
 
   const [isLoading, setIsLoading] = useState(true)
-  const [activities, setActivities] = useState({})
+  const [activities, setActivities] = useState([]);
+  const [athlete, setAthlete] = useState({});
 
-  //Strava Credentials
-  let clientID = "113235";
-  let clientSecret = "4069d12e70c672a834bf7161d0652abd413c92d6";
-
-  // refresh token and call address
+  // Strava Credentials
+  const clientID = "113235";
+  const clientSecret = "4069d12e70c672a834bf7161d0652abd413c92d6";
   const refreshToken = "a93df7afd0f962f71ed0ec01948c9ece96ff1a72";
-  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`
-  
-  // endpoint for read-all activities. temporary token is added in getActivities()
-  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`
 
-  // Use refresh token to get current access token
+  // Endpoints for read-all activities and athlete data
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
+  const callActivities = "https://www.strava.com/api/v3/athlete/activities"; // Do not include access_token here
+  const callAthlete = "https://www.strava.com/api/v3/athlete";
+
   useEffect(() => {
-    fetch(callRefresh, {
-      method: 'POST'
-    })
-    .then(res => res.json())
-    .then(result => getActivities(result.access_token))
-  }, [callRefresh])
+    function getActivities(access) {
+      fetch(`${callActivities}?access_token=${access}`)
+        .then((res) => res.json())
+        .then((data) => setActivities(data))
+        .catch((e) => console.log(e));
+    }
 
-  // use current access token to call all activities
-  function getActivities(access){
-      // console.log(callActivities + access)
-        fetch(callActivities + access)
-        .then(res => res.json())
-        .then(data => {
-          setActivities(data); // set the data first
-          setIsLoading(prev => !prev); // then change the loading state
-        })
-        .catch(e => console.log(e))
+    function getAthleteData(access) {
+      fetch(`${callAthlete}?access_token=${access}`)
+        .then((res) => res.json())
+        .then((data) => setAthlete(data))
+        .catch((e) => console.log(e));
+    }
+
+    fetch(callRefresh, {
+      method: 'POST',
+    })
+      .then((res) => res.json())
+      .then((result) => {
+        getActivities(result.access_token);
+        getAthleteData(result.access_token);
+        setIsLoading(false);
+      });
+  }, [callRefresh]);
+
+  function showActivities() {
+    if (isLoading) return <p>LOADING...</p>;
+    if (!isLoading) {
+      console.log(activities);
+      return activities.length;
+    }
   }
 
-
-  function showActivities(){
-    if(isLoading) return <>LOADING</>
-    if(!isLoading) {
-      console.log(activities)
-      return activities.length
+  function showAthleteData() {
+    if (isLoading) return <p>LOADING...</p>;
+    if (!isLoading) {
+      console.log(athlete);
+      return (
+        <div>
+          <p>Name: {athlete.firstname} {athlete.lastname}</p>
+          <p>Bio: {athlete.bio}</p>
+          <p>City: {athlete.city}</p>
+          {/* Add more athlete data as needed */}
+        </div>
+      );
     }
   }
 
   return (
     <div className="App">
       <Header />
+      <div>
+        <h2>Your Activities</h2>
         {showActivities()}
+      </div>
+      <div>
+        <h2>Your Profile</h2>
+        {showAthleteData()}
+      </div>
     </div>
   );
 }
