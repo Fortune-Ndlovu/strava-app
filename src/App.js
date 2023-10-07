07fa6cd27169df399630edbb746a6be05b0c7465
Fortune Ndlovu <ndlovufortune97@gmail.com>
2023-10-08 00:36:04 +0100

    STRAV-4 Integrate Data From The Strava API In Your React App epic complete

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+// Fundamental for creating components, managing state, and handling side effects.
 import React, { useState, useEffect } from 'react';
-import Header from './components/Header';
+
+// Destructuring the getActivities and getAthleteData: Functions for fetching data from the Strava API.
 import { getActivities, getAthleteData } from './stravaApi';
+
+// Configuration data required for Strava API authentication and access.
 import config from './config.json';
+import Header from './components/Header';
+import Q from 'q';
 
 function App() {
-  // State variables
   const [isLoading, setIsLoading] = useState(true);
   const [activities, setActivities] = useState([]);
   const [athlete, setAthlete] = useState({});
@@ -16,31 +21,39 @@ function App() {
     // fetching data from the Strava API.
     async function fetchData() {
       try {
+        // Constructing a URL using the API credentials to obtain an access token via a POST request.
         const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
 
         // Constructing the 'callRefresh' constant url using the Strava API credentials to obtain the access token by making a POST request.
         const responseToken = await fetch(callRefresh, { method: 'POST' });
 
+        // Handling the response token if it is not ok throw an error
         if (!responseToken.ok) {
           throw new Error('Failed to obtain access token');
         }
 
+        // Parsing the responseToken to json so we can access the access_token using dot notation passing it to the,
+        // getActivities and getAthleteData functions that receive it as an argument as uses the accessToken to fetch for relevant data and brings it back.
         const result = await responseToken.json();
-
         const activitiesData = await getActivities(result.access_token);
         const athleteData = await getAthleteData(result.access_token);
 
+        // Once we have the received the data back from the server we plug it into the useState functions that pass it to the state variable for later use.
         setActivities(activitiesData);
         setAthlete(athleteData);
         setIsLoading(false);
       } catch (error) {
+        // if there is an error catch it and log it out into the console
         console.error('Error fetching data:', error);
         setIsLoading(false);
       }
     }
-
+    
+    // kick off the data-fetching process whenever any of the specified dependencies (clientID, clientSecret, or refreshToken) change.
+    // This ensures that the data is fetched and updated whenever these important parameters change.
     fetchData();
-  }, [clientID, clientSecret, refreshToken]);
+
+  }, [clientID, clientSecret, refreshToken]); //When any of these dependencies change, the useEffect hook will re-run.
 
   function showActivities() {
     if (isLoading) return <p>LOADING...</p>;
