530b166a76e40e6b4564ff9222753105a7b0ba64
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-01-10 17:19:31 +0000

    STRAV-30 made the creating of new activities fast bynow using onSnapshot function t listen for real-time updates from firebase. It will automatically re-render whenever there is a change in the users collection

--- src/services/UserActivitiesManager.js
+++ src/services/UserActivitiesManager.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { db } from "../firebase/firebase";
-import { collection, getDocs, addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc, onSnapshot } from "firebase/firestore";
 import ManualEntryForm from "../components/ManualEntryForm/ManualEntryForm";
 import MyActivitiesTable from "../components/MyActivitiesTable/MyActivitiesTable";
 
@@ -13,12 +13,21 @@ const UserActivitiesManager = () => {
   };
 
   useEffect(() => {
-    const getUsers = async () => {
-      const data = await getDocs(usersCollectionRef);
-      setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
+   const getUsers = async () => {
+     const data = await getDocs(usersCollectionRef);
+     setUsers(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+   };
+
+   const unsubscribe = onSnapshot(usersCollectionRef, (snapshot) => {
+    // Update the state whenever there's a change in the collection
+    setUsers(snapshot.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+  });
+
+  // Cleanup the listener when the component unmounts
     getUsers();
-  }, []);
+    return () => unsubscribe();
+  }, []); // Empty dependency array to run the effect only once on mount
+
 
   return (
     <div>
