231b4dd26de4e2418f6308f7eb75c90d92fa5409
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-01-14 10:03:36 +0000

    Tabs Refractor

---
+++ src/components/SideBar/LeftDashboardAthleteSidebarComponents/TabsCard.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { MdDoubleArrow } from "react-icons/md";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import WeeksStyleSvg from "./WeeksStyleSvg";
+
+const TabsCard = ({ svgContent }) => {
+	return (
+		<div>
+			{" "}
+			{/* second tab body */}
+			<div className="relativeEffortTitleStyle">
+				<MdDoubleArrow className="arrowIcon" />
+				<p>GOALS</p>
+			</div>
+			<Card style={{ width: "18rem" }} id="recoveryWeek-cardStyle">
+				<Card.Body>
+					<Card.Text className="goals-tab-cardText">
+						<div className="goals-tab-text">
+							<p>
+								Subscribe to stay motivated with custom progress, segment and
+								power goals. <a href="home">Upgrade</a>
+							</p>
+						</div>
+					</Card.Text>
+					<Card.Text className="weekStats-cardText">
+						<div className="goals-tab-stats">
+							<p className="thisWeek-para">THIS WEEK</p>
+							<p className="thisWeek-numbers">0 / 0 km</p>
+						</div>
+					</Card.Text>
+					<Card.Text>
+                        <div className="tab-stats" style={{ display: "flex", alignItems: "center", justifyContent: "center"}}>
+                            <WeeksStyleSvg />
+                            {svgContent}
+						</div>
+					</Card.Text>
+					<Card.Text className="weekStats-cardText">
+						<div className="goals-tab-progress">
+							<span id="rangeSpan">--:--</span>
+							<span id="mileSpan">0 m</span>
+						</div>
+					</Card.Text>
+					<Card.Text className="weekStats-cardText-second">
+						<div className="goals-tab-stats">
+							<p className="thisWeek-para">THIS YEAR</p>
+							<p className="thisWeek-numbers">0 / 0 km</p>
+						</div>
+					</Card.Text>
+				</Card.Body>
+				<Card.Body className="training-log">
+					<Card.Link href="home" id="goals-log-link">
+						Manage Your Goals
+						<RiArrowDropDownLine className="trainingLogIcon" />
+					</Card.Link>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
+
+export default TabsCard;
---
+++ src/components/SideBar/LeftDashboardAthleteSidebarComponents/WeeksStyleSvg.jsx
@@ -0,0 +1,212 @@
+import React from "react";
+
+const WeeksStyleSvg = () => {
+	return (
+		<div>
+			{" "}
+			<svg
+				className="recharts-surface"
+				width="150"
+				height="100"
+				viewBox="0 0 150 100"
+			>
+				<title></title>
+				<desc></desc>
+				<defs>
+					<clipPath id="recharts1-clip">
+						<rect x="5" y="5" height="60" width="140"></rect>
+					</clipPath>
+				</defs>
+				<g className="recharts-layer recharts-bar">
+					<g className="recharts-layer recharts-bar-rectangles">
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="7"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 7,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="27"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 27,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="47"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 47,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="67"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 67,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="87"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 87,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="107"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 107,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+						<g className="recharts-layer recharts-bar-rectangle">
+							<path
+								fill="#d3d1cd"
+								width="16"
+								height="2"
+								x="127"
+								y="63"
+								radius="0"
+								className="recharts-rectangle"
+								d="M 127,63 h 16 v 2 h -16 Z"
+							></path>
+						</g>
+					</g>
+					<g className="recharts-layer"></g>
+				</g>
+				<g className="recharts-layer recharts-cartesian-axis recharts-xAxis xAxis">
+					<g className="recharts-cartesian-axis-ticks">
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(15,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									M
+								</text>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(35,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									T
+								</text>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(55,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									W
+								</text>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(75,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="800"
+								>
+									T
+								</text>
+								<svg x="-5" y="15" width="10" height="10">
+									<polygon points="5,0 0,10 10,10" fill="#fc5200"></polygon>
+								</svg>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(95,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									F
+								</text>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(115,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									S
+								</text>
+							</g>
+						</g>
+						<g className="recharts-layer recharts-cartesian-axis-tick">
+							<g transform="translate(135,73)">
+								<text
+									x="0"
+									y="10"
+									fill="#2b2b2b"
+									textAnchor="middle"
+									fontWeight="400"
+								>
+									S
+								</text>
+							</g>
+						</g>
+					</g>
+				</g>
+			</svg>
+		</div>
+	);
+};
+
+export default WeeksStyleSvg;
