74492a910645f56592fa4bb41013a3f554f90e43
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-01-17 22:49:27 +0000

    Default React Bootstrap Code Refractor

--- src/components/ManualEntryForm/ManualEntryFormComponents/ActivityStats.jsx
+++ src/components/ManualEntryForm/ManualEntryFormComponents/ActivityStats.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
 import Form from "react-bootstrap/Form";
@@ -18,85 +18,112 @@ const ActivityStats = ({
 	elevationValue,
 	elevationOnChange,
 }) => {
+	const [selectedDistanceUI, setSelectedUDistanceUI] = useState("Kilometers");
+	const [selectedElevationUI, setSelectedElevationUI] = useState("Meters");
+
+	const handleSelectedDistanceUI = (value) => {
+		setSelectedUDistanceUI(value);
+	};
+
+	const handleSelectedElevationUI = (value) => {
+		setSelectedElevationUI(value);
+	};
+
 	return (
 		<div>
-				<Row>
-					<Col className="activity-stats-col">
-						<Form.Label>Distance</Form.Label>
-						<InputGroup className="mb-3">
-							<Form.Control
-								aria-label="Text input with dropdown button"
-								type="number"
-								value={distanceValue}
+			<Row>
+				<Col className="activity-stats-col">
+					<Form.Label>Distance</Form.Label>
+					<InputGroup id="distanceInputWrapper">
+						<Form.Control
+							aria-label="Text input with dropdown button"
+							type="number"
+							value={distanceValue}
 							onChange={distanceOnChange}
 							id="distanceInput"
-							/>
-							<DropdownButton
-								variant="outline-secondary"
-								title="Dropdown"
-								id="input-group-dropdown-2"
-								align="end"
+						/>
+						<DropdownButton
+							variant="outline-secondary"
+							title={selectedDistanceUI}
+							id="distanceDropdown"
+							align="end"
+						>
+							<Dropdown.Item
+								onClick={() => handleSelectedDistanceUI("Kilometers")}
 							>
-								<Dropdown.Item href="#">Kilometers</Dropdown.Item>
-								<Dropdown.Item href="#">Meters</Dropdown.Item>
-								<Dropdown.Item href="#">miles</Dropdown.Item>
-								<Dropdown.Item href="#">yards</Dropdown.Item>
-							</DropdownButton>
-						</InputGroup>
-					</Col>
+								Kilometers
+							</Dropdown.Item>
+							<Dropdown.Item onClick={() => handleSelectedDistanceUI("Meters")}>
+								Meters
+							</Dropdown.Item>
+							<Dropdown.Item onClick={() => handleSelectedDistanceUI("Miles")}>
+								Miles
+							</Dropdown.Item>
+							<Dropdown.Item onClick={() => handleSelectedDistanceUI("Yard")}>
+								Yards
+							</Dropdown.Item>
+						</DropdownButton>
+					</InputGroup>
+				</Col>
 
-					<Col className="activity-stats-col">
-						<Form.Label>Duration</Form.Label>
-						<InputGroup className="mb-3" type="number">
-							<Form.Control
-								aria-label="hour"
-								type="number"
-								placeholder="hr"
-								value={hourValue}
+				<Col className="activity-stats-col">
+					<Form.Label>Duration</Form.Label>
+					<InputGroup className="mb-3" type="number">
+						<Form.Control
+							aria-label="hour"
+							type="number"
+							placeholder="hr"
+							value={hourValue}
 							onChange={hourOnChange}
 							id="duration-input-hr"
-							/>
-							<Form.Control
-								aria-label="minute"
-								type="number"
-								placeholder="min"
-								value={minuteValue}
+						/>
+						<Form.Control
+							aria-label="minute"
+							type="number"
+							placeholder="min"
+							value={minuteValue}
 							onChange={minuteOnChange}
 							id="duration-input-min"
-							/>
-							<Form.Control
-								aria-label="second"
-								type="number"
-								placeholder="s"
-								value={secondValue}
+						/>
+						<Form.Control
+							aria-label="second"
+							type="number"
+							placeholder="s"
+							value={secondValue}
 							onChange={secondOnChange}
 							id="duration-input-s"
-							/>
-						</InputGroup>
-					</Col>
-					<Col className="activity-stats-col">
-						<Form.Label>Elevation</Form.Label>
-						<InputGroup className="mb-3">
-							<Form.Control
-								aria-label="Text input with dropdown button"
-								type="number"
-								value={elevationValue}
+						/>
+					</InputGroup>
+				</Col>
+				<Col className="activity-stats-col">
+					<Form.Label>Elevation</Form.Label>
+					<InputGroup className="mb-3" id="elevationInputWrapper">
+						<Form.Control
+							aria-label="Text input with dropdown button"
+							type="number"
+							value={elevationValue}
 							onChange={elevationOnChange}
 							id="elevation-input"
-							/>
-							<DropdownButton
-								variant="outline-secondary"
-								title="Dropdown"
-								id="input-group-dropdown-2"
+						/>
+						<DropdownButton
+							variant="outline-secondary"
+							title={selectedElevationUI}
+							id="elevationDropdown"
 							align="end"
 							className="duration-input"
+						>
+							<Dropdown.Item
+								onClick={() => handleSelectedElevationUI("Meters")}
 							>
-								<Dropdown.Item href="#">Meters</Dropdown.Item>
-								<Dropdown.Item href="#">feet</Dropdown.Item>
-							</DropdownButton>
-						</InputGroup>
-					</Col>
-				</Row>
+								Meters
+							</Dropdown.Item>
+							<Dropdown.Item onClick={() => handleSelectedElevationUI("Feet")}>
+								Feet
+							</Dropdown.Item>
+						</DropdownButton>
+					</InputGroup>
+				</Col>
+			</Row>
 		</div>
 	);
 };
--- src/components/UserActivitiesDropdownToggle/UserActivitiesDropdownToggle.jsx
+++ src/components/UserActivitiesDropdownToggle/UserActivitiesDropdownToggle.jsx
@@ -9,17 +9,16 @@ const UserActivitiesDropdownToggle = () => {
 	const handleSelectedActivityUIChange = (value) => {
 		setSelectedUserActivityUI(value);
 	};
+
 	return (
 		<div>
 			<Dropdown id="userActivities">
 				<Dropdown.Toggle variant="success" id="userActivities-dropdownBtn-list">
 					{selectedUserActivityUI}
-                    {/* <img src={dropdown_icon} alt="user activities dropdown toggle dropdown icon" className="feedUI-dropdownUp-icon" /> */}
                         <svg className="feedUI-dropdownUp-icon" height="20" width="20" viewBox="0 0 20 20" aria-hidden="true" focusable="false" class="css-8mmkcg"><path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path></svg>
 				</Dropdown.Toggle>
 				<Dropdown.Menu id="userActivities-btn-list-dropdown">
 					<Dropdown.Item
-						href="#/action-1"
 						className="userActivities-dropDown-link"
 						onClick={() => handleSelectedActivityUIChange("Following")}
 						value="Following"
@@ -27,7 +26,6 @@ const UserActivitiesDropdownToggle = () => {
 						Following
 					</Dropdown.Item>
 					<Dropdown.Item
-						href="#/action-1"
 						className="userActivities-dropDown-link"
 						onClick={() =>
 							handleSelectedActivityUIChange("Waterford Viking Marathon")
@@ -37,7 +35,6 @@ const UserActivitiesDropdownToggle = () => {
 						Waterford Viking Marathon
 					</Dropdown.Item>
 					<Dropdown.Item
-						href="#/action-2"
 						className="userActivities-dropDown-link"
 						onClick={() => handleSelectedActivityUIChange("WIT Arena")}
 						value="WIT Arena"
