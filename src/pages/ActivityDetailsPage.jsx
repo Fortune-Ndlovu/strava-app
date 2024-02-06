3f7aa9c1eecc740190110af4a9e131cea535f778
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-02-06 15:03:04 +0000

    React BootStrap Default UI Overide

--- src/pages/ActivityDetailsPage.jsx
+++ src/pages/ActivityDetailsPage.jsx
@@ -7,11 +7,17 @@ import ButtonGroup from "react-bootstrap/ButtonGroup";
 import Dropdown from "react-bootstrap/Dropdown";
 import Accordion from "react-bootstrap/Accordion";
 import { Container, Row, Col } from "react-bootstrap";
+import { FaFacebookF } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { IoShareSocialOutline } from "react-icons/io5";
+import { IoMdArrowDropdown } from "react-icons/io";
 import "../styles/ActivityDetailsPage.css";
 import "../styles/common/buttons.css";
+import fortunendlovu from "../images/fortunendlovu.jpg";
 
 const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-	<a id="activity-details-dropside-btn"
+	<a
+		id="activity-details-dropside-btn"
 		title="Actions"
 		href="#"
 		ref={ref}
@@ -113,46 +119,107 @@ const ActivityDetailsPage = () => {
 								<Accordion defaultActiveKey="0" id="activityDetailPageBook">
 									<Accordion.Item eventKey="0">
 										<Accordion.Header id="activity-details-book-header">
-											<div id="activityDetailsTypeInfo">
-												<a href="#">Fortune Ndlovu </a> - {activityDetails.sport}
-											</div>
-											<ButtonGroup aria-label="Basic example" id="socials">
-												<Button variant="secondary">Left</Button>
-												<Button variant="secondary">Middle</Button>
-												<Button variant="secondary">Right</Button>
-											</ButtonGroup>
+											<Row id="activity-details-book-header-Row">
+												<Col xs="9" id="activity-details-book-header-Col">
+													<div id="activityDetailsTypeInfo">
+														<a href="/activities">Fortune Ndlovu </a> -{" "}
+														{activityDetails.sport}
+													</div>
+												</Col>
+												<Col xs="3">
+													<ButtonGroup aria-label="Basic example" id="socials">
+														<Button variant="secondary">
+															<FaFacebookF id="facebook-icon" />
+														</Button>
+														<Button variant="secondary">
+															<FaTwitter id="twitter-icon" />
+														</Button>
+														<Button variant="secondary">
+															<IoShareSocialOutline id="share-icon" />
+														</Button>
+														<Button
+															variant="secondary"
+															className="activity-details-kudos"
+														>
+															<svg
+																fill="currentColor"
+																xmlns="http://www.w3.org/2000/svg"
+																viewBox="0 0 16 16"
+																width="16"
+																height="16"
+																data-testid="unfilled_kudos"
+															>
+																<path
+																	d="M15.243 7.182a1.907 1.907 0 00-.532-1.423 2.069 2.069 0 00-1.493-.641H9.863l.454-1.812A2.426 2.426 0 008.064.514h-.513l-.718 2.807L4.97 6.915.412 9.34l2.472 6.424 4.278-2.28h4.785a2.142 2.142 0 002.127-1.976l.084-1.177a1.962 1.962 0 00.712-2.097 1.93 1.93 0 00.373-1.052zM1.664 9.807l2.06-1.1 1.748 4.542-2.061 1.1-1.747-4.542zm12.289-2.038l-.268.254.165.331a.942.942 0 01-.044.903.965.965 0 01-.369.352l-.237.131-.122 1.7a1.123 1.123 0 01-1.129 1.049H6.914l-.552.295-1.748-4.547 1.1-.586 2.033-3.92.567-2.166a1.427 1.427 0 011.032 1.371c0 .071 0 .139-.007.167l-.758 3.016h4.64a1.059 1.059 0 01.764.328.917.917 0 01.26.683.942.942 0 01-.292.639z"
+																	fill=""
+																></path>
+															</svg>
+															0
+														</Button>
+														<Button
+															variant="secondary"
+															className="activity-details-kudos"
+														>
+															<svg
+																fill="currentColor"
+																xmlns="http://www.w3.org/2000/svg"
+																viewBox="0 0 16 16"
+																width="16"
+																height="16"
+															>
+																<path
+																	d="M.5 1.5v11h2v3.21l6.06-3.21h6.94v-11H.5zm14 10H8.31L3.5 14.05V11.5h-2v-9h13v9z"
+																	fill=""
+																></path>
+																<path
+																	d="M12.96 4.5H3v1h9.43l.53-1zm-1.59 3H3v1h7.84l.53-1z"
+																	fill=""
+																></path>
+															</svg>
+															0
+														</Button>
+														<Button variant="secondary">
+															<IoMdArrowDropdown className="activity-details-toggle-icon" />
+														</Button>
+													</ButtonGroup>
+												</Col>
+											</Row>
 										</Accordion.Header>
 										<Accordion.Body>
-											<p>Date of the activity: {activityDetails.date}</p>
-											<p>Time of the activity: {activityDetails.time}</p>
-											<p>Name: {activityDetails.name}</p>
-											<p>Description: {activityDetails.description}</p>
-											<p>
-												The activity Duration: {activityDetails.hour}:
-												{activityDetails.minute}:{activityDetails.second}
-											</p>
-											{activityDetails.imageUrls &&
-												activityDetails.imageUrls.length > 0 && (
-													<div>
-														<p>Images:</p>
+											<div>
+												<p>Date of the activity: {activityDetails.date}</p>
+												<p>Time of the activity: {activityDetails.time}</p>
+												<img src={fortunendlovu} alt="fortune ndlovu" width={50} height={50}/>
+												<p>Name: {activityDetails.name}</p>
+												<p>Description: {activityDetails.description}</p>
+												{activityDetails.imageUrls &&
+													activityDetails.imageUrls.length > 0 && (
 														<div>
-															{activityDetails.imageUrls.map(
-																(imageUrl, index) => (
-																	<img
-																		key={index}
-																		src={imageUrl}
-																		alt={`Activity ${index + 1}`}
-																		width={64}
-																		height={64}
-																		style={{ marginRight: "10px" }}
-																		loading="lazy"
-																	/>
-																)
-															)}
+															<div>
+																{activityDetails.imageUrls.map(
+																	(imageUrl, index) => (
+																		<img
+																			key={index}
+																			src={imageUrl}
+																			alt={`Activity ${index + 1}`}
+																			width={64}
+																			height={64}
+																			style={{ marginRight: "10px" }}
+																			loading="lazy"
+																		/>
+																	)
+																)}
+															</div>
 														</div>
-													</div>
-												)}
-											<p>Shoes: Nike NIKE W NIKE METCON 8 BLACK (0.6 km)</p>
+													)}
+											</div>
+											<div>
+												<p>
+													The activity Duration: {activityDetails.hour}:
+													{activityDetails.minute}:{activityDetails.second}
+												</p>
+												<p>Shoes: Nike NIKE W NIKE METCON 8 BLACK (0.6 km)</p>
+											</div>
 										</Accordion.Body>
 									</Accordion.Item>
 								</Accordion>
