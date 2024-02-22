d2251911db651393ad95011f0d87de78a7f97fb1
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-02-22 07:46:36 +0000

    Controlling the textarea form element by updating the state with the value of the textarea

--- src/components/CreateCommentsAndGiveKudos/CommentSection.jsx
+++ src/components/CreateCommentsAndGiveKudos/CommentSection.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { React, useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import "./CreateCommentsAndGiveKudos.css";
@@ -6,19 +6,16 @@ import "../../styles/common/buttons.css";
 import fortunendlovu from "../../images/fortunendlovu.jpg";
 
 const CommentSection = ({ showComments, onCommentPost }) => {
+	const [comment, setComment] = useState("");
 
 	const handleCommentPost = () => {
-		// Get the comment from the textarea
-		const comment = document.getElementById("commentingTextAreaInput").value;
-
 		// Call the parent component's onCommentPost callback with the new comment
 		onCommentPost(comment);
-
 		// Clear the textarea after posting the comment
-		document.getElementById("commentingTextAreaInput").value = "";
+		setComment("");
 	};
-	
-    return (
+
+	return (
 		<div
 			className={`comment-section ${showComments ? "visible" : "hidden"}`}
 			id="activityCommentsWrapper"
@@ -36,9 +33,15 @@ const CommentSection = ({ showComments, onCommentPost }) => {
 					id="commentingTextAreaInput"
 					placeholder="Add a comment, @ to mention"
 					rows={2}
+					value={comment}
+					onChange={(e) => setComment(e.target.value)}
 				/>
 			</Form.Group>
-			<Button variant="outline-primary" id="activityCommentPostBtn" onClick={handleCommentPost}>
+			<Button
+				variant="outline-primary"
+				id="activityCommentPostBtn"
+				onClick={handleCommentPost}
+			>
 				Post
 			</Button>{" "}
 		</div>
