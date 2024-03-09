88cb017577bec4c51c02f876eb36130faa5a4755
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-09 16:23:28 +0000

    Directing the user to the home page upon successful signup.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,38 +1,58 @@
-import { React, useState } from 'react';
+import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
-import authSignUpWithEmailAndPassword from '../services/authSignUpWithEmailAndPassword';
+import authSignUpWithEmailAndPassword from "../services/authSignUpWithEmailAndPassword";
 import "../styles/LogInSignUp.css";
 
 const SignUp = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
+	const navigate = useNavigate();
 
-	const handleSignUp = async (e) => { 
+	const handleSignUp = async (e) => {
 		e.preventDefault();
-        const user = await authSignUpWithEmailAndPassword(email, password);
+		const user = await authSignUpWithEmailAndPassword(email, password);
 		console.log("The user is being Signed Up: ", user);
-	}
 
-  return (
-   <div className='LogInSignUp'>
+		if (user && !user.error) {
+			//Successfully signed up
+			navigate("/");
+		} else {
+			//Failed to sign up
+			console.error(user.error);
+		}
+	};
+
+	return (
+		<div className="LogInSignUp">
 			<h1>Sign Up</h1>
-		  <Form onSubmit={handleSignUp}>
+			<Form onSubmit={handleSignUp}>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
+					<Form.Control
+						type="email"
+						placeholder="Enter email"
+						value={email}
+						onChange={(e) => setEmail(e.target.value)}
+					/>
 				</Form.Group>
 
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
-				  <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+					<Form.Control
+						type="password"
+						placeholder="Password"
+						value={password}
+						onChange={(e) => setPassword(e.target.value)}
+					/>
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Sign Up
 				</Button>
 			</Form>
 		</div>
-  )
-}
+	);
+};
 
-export default SignUp
+export default SignUp;
