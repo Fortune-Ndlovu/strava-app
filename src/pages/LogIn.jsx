857489708ce65f6604eced2525e6c49822af6332
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-09 16:09:44 +0000

    Handling the form input upon submission

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -11,7 +11,7 @@ const LogIn = () => {
 	const handleLogin = async (e) => { 
 		e.preventDefault();
 		const user = await authSignInWithEmailAndPassword(email, password);
-		console.log(user);
+		console.log("The user is being Logged in: ", user);
 	}
 
 	return (
@@ -20,12 +20,12 @@ const LogIn = () => {
 			<Form onSubmit={handleLogin}>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
+					<Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e)} />
 				</Form.Group>
 
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
+					<Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e)} />
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Log In
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -11,7 +11,7 @@ const SignUp = () => {
 	const handleSignUp = async (e) => { 
 		e.preventDefault();
         const user = await authSignUpWithEmailAndPassword(email, password);
-		console.log(user);
+		console.log("The user is being Signed Up: ", user);
 	}
 
   return (
@@ -20,12 +20,12 @@ const SignUp = () => {
 		  <Form onSubmit={handleSignUp}>
 				<Form.Group className="mb-3" controlId="formBasicEmail">
 					<Form.Label>Email address</Form.Label>
-					<Form.Control type="email" placeholder="Enter email" />
+					<Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)}/>
 				</Form.Group>
 
 				<Form.Group className="mb-3" controlId="formBasicPassword">
 					<Form.Label>Password</Form.Label>
-					<Form.Control type="password" placeholder="Password" />
+				  <Form.Control type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
 				</Form.Group>
 				<Button variant="primary" type="submit">
 					Sign Up
