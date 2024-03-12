1e4106a1636adb7fc71966d90200d9c9c83799d2
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-12 21:18:09 +0000

    Compressing the image before uploading it storage

--- src/components/UserProfileForm/ProfileComponent.jsx
+++ src/components/UserProfileForm/ProfileComponent.jsx
@@ -1,50 +1,51 @@
 import { useState } from "react";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { db } from "../../firebase/firebase";
-import { doc, updateDoc } from "firebase/firestore";
-import { getCurrentUserId } from "../../firebase/firebase";
+import { compressImage } from "../../services/compressImage"; // Import the compressImage function
 
 const ProfileComponent = ({ updateUserDocument }) => {
-  const [selectedImage, setSelectedImage] = useState(null);
+	const [selectedImage, setSelectedImage] = useState(null);
 
-  const handleImageChange = async (e) => {
-    const file = e.target.files[0];
-    setSelectedImage(file);
+	const handleImageChange = async (e) => {
+		const file = e.target.files[0];
+		setSelectedImage(file);
 
-    // Upload image to Firebase Storage
-    const storage = getStorage();
-    const storageRef = ref(storage, `profile_images/${file.name}`);
-    await uploadBytes(storageRef, file);
+		// Compress the image before uploading
+		const compressedImage = await compressImage(file);
 
-    // Get download URL of the uploaded image
-    const imageUrl = await getDownloadURL(storageRef);
+		// Upload compressed image to Firebase Storage
+		const storage = getStorage();
+		const storageRef = ref(storage, `profile_images/${file.name}`);
+		await uploadBytes(storageRef, compressedImage);
 
-    // Update the user document with the image URL
-    try {
-      await updateUserDocument({ profileImageUrl: imageUrl });
-      console.log("Profile image updated successfully.");
-    } catch (error) {
-      console.error("Error updating profile image:", error);
-    }
-  };
+		// Get download URL of the uploaded image
+		const imageUrl = await getDownloadURL(storageRef);
 
-  return (
-    <div>
-      <input type="file" onChange={handleImageChange} />
-      {selectedImage && (
-        <img
-          src={URL.createObjectURL(selectedImage)}
-          alt="Selected Image"
-          width={70}
-          height={70}
-          style={{
-            objectFit: "cover",
-            marginRight: "10px",
-          }}
-        />
-      )}
-    </div>
-  );
+		// Update the user document with the image URL
+		try {
+			await updateUserDocument({ profileImageUrl: imageUrl });
+			console.log("Profile image updated successfully.");
+		} catch (error) {
+			console.error("Error updating profile image:", error);
+		}
+	};
+
+	return (
+		<div>
+			<input type="file" onChange={handleImageChange} />
+			{selectedImage && (
+				<img
+					src={URL.createObjectURL(selectedImage)}
+					alt="Selected Image"
+					width={70}
+					height={70}
+					style={{
+						objectFit: "cover",
+						marginRight: "10px",
+					}}
+				/>
+			)}
+		</div>
+	);
 };
 
 export default ProfileComponent;
