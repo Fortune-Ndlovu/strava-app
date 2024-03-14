2b705c8d89fde8a7c5943be35a3672c5af6daffc
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-14 00:19:59 +0000

    Sign Up page footer styling

--- src/components/SignUpFooter/SignUpFooter.jsx
+++ src/components/SignUpFooter/SignUpFooter.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
-import stravaLogoFooter from "../../images/signUp-strava-logo.png";
+import { FaTwitter } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { CiInstagram } from "react-icons/ci";
+import { FaLinkedin } from "react-icons/fa";
+import { FaFacebookSquare } from "react-icons/fa";
+import getItOnGooglePlayLogo from "../../images/Get_it_on_Google_Play_Logo.png";
+import stravaLogoFooter from "../../images/strava_logo.svg";
+import DownloadOnTheAppleStore from "../../images/Download_on_the_App_Store_logo.png";
+import "./SignUpFooter.css";
 const SignUpFooter = () => {
 	return (
 		<div>
@@ -9,132 +17,124 @@ const SignUpFooter = () => {
 					<div className="container footerMaxWidth">
 						<div className="row">
 							<div className="col-md-2">
-								<Navbar.Brand
-									title="Return to the Strava home page"
-									href="#home"
-									id="footer-strava-logo"
-								>
-									<img
-										src={stravaLogoFooter}
-										id="strava-logo-footer"
-										alt="Company brand logo that simply says strava."
-										width={200}
-									/>
-								</Navbar.Brand>
-								<p>&copy; 2023 Strava</p>
-								{/* You can replace this with the Strava logo */}{" "}
-							</div>
-							<div className="col-md-2">
-								<h5>About</h5>
 								<ul className="list-unstyled">
 									<li>
-										<a href="/about">About</a>
+										<img
+											src={stravaLogoFooter}
+											id="strava-logo-footer"
+											alt="Company brand logo that simply says strava."
+											width={200}
+										/>
 									</li>
 									<li>
-										<a href="/features">Features</a>
+										<p>
+											Strava protects your data. Read more in our <a href="#Privacy Policy">Privacy Policy.</a>
+										</p>
 									</li>
+									<div className="signUpFooterIcons">
+
 									<li>
-										<a href="/mobile">Mobile</a>
-									</li>
-									<li>
-										<a href="/subscribe?cta=subscription&amp;element=nav&amp;source=global_footer">
-											Subscription
+										<a href="/events/boston-marathon">
+											<FaTwitter />
 										</a>
 									</li>
 									<li>
-										<a href="/student?origin=global_footer">Student Discount</a>
+										<a href="/events/london-marathon">
+											<FaYoutube />
+										</a>
 									</li>
 									<li>
-										<a href="/legal/privacy">Privacy Policy</a>
+										<a href="/events/nyc-marathon">
+											<CiInstagram />
+										</a>
 									</li>
 									<li>
-										<a href="#home">Do Not Share My Personal Information</a>
+										<a href="/routes/hiking/usa">
+											<FaLinkedin />
+										</a>
 									</li>
 									<li>
-										<a href="/legal/terms">Terms and Conditions</a>
+										<a href="/events/paris-marathon">
+											<FaFacebookSquare />
+										</a>
+									</li>
+									</div>
+								</ul>
+							</div>
+							<div className="col-md-2">
+								<ul className="list-unstyled">
+									<li>
+										<a href="/events/boston-marathon">Features</a>
 									</li>
 									<li>
-										<a href="https://support.strava.com/hc/en-us/articles/216917717-About-Strava-Maps">
-											About Our Maps
+										<a href="/events/london-marathon">Subscription</a>
+									</li>
+									<li>
+										<a href="/events/nyc-marathon">Student Discount</a>
+									</li>	<li>
+										<a href="/events/nyc-marathon">About</a>
+									</li>	<li>
+										<a href="/events/nyc-marathon">Careers</a>
+									</li>	<li>
+										<a href="/events/nyc-marathon">Press</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-md-2">
+								<ul className="list-unstyled">
+									<li>
+										<a href="#home">Routes</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-md-2">
+								<ul className="list-unstyled">
+									<li>
+										<a href="https://strava.zendesk.com/home">What's New</a>
+									</li>
+									<li>
+
+										<a href="https://strava.zendesk.com/home">Stories</a>
+									</li>
+									<li>
+									<a href="https://strava.zendesk.com/home">Support</a>
+									</li>	
+									<li>
+
+									<a href="https://strava.zendesk.com/home">Business</a>
+									</li>
+									<li>
+
+										<a href="https://strava.zendesk.com/home">Partner Center</a>
+									</li>
+									<li>
+										<a href="https://strava.zendesk.com/home">Terms</a>
+									</li>
+								</ul>
+							</div>
+							<div className="col-md-2">
+								<ul className="list-unstyled">
+									<li>
+										<a href="/careers">Privacy</a>
+									</li>
+									<li>
+										<a href="https://press.strava.com">Do Not Share My Personal Information</a>
+									</li>
+									<li>
+										<a href="https://business.strava.com?utm_source=footer&amp;utm_medium=referral" className="signUpFooterLink">
+											Log In
 										</a>
 									</li>
 								</ul>
 							</div>
 							<div className="col-md-2">
-								<h5>Explore</h5>
 								<ul className="list-unstyled">
 									<li>
-										<a href="/routes/hiking/usa">Routes</a>
+										<a href="/DownloadOnTheAppleStore"><img src={DownloadOnTheAppleStore} alt="Apple Store Logo" height={35}/></a>
 									</li>
+									<br></br>
 									<li>
-										<a href="/events/paris-marathon">Paris 2023 Marathon</a>
-									</li>
-									<li>
-										<a href="/events/boston-marathon">Boston 2023 Marathon</a>
-									</li>
-									<li>
-										<a href="/events/london-marathon">London 2023 Marathon</a>
-									</li>
-									<li>
-										<a href="/events/nyc-marathon">NYC 2023 Marathon</a>
-									</li>
-								</ul>
-							</div>
-							<div className="col-md-2">
-								<h5>Follow</h5>
-								<ul className="list-unstyled">
-									<li>
-										<a href="#home">Facebook</a>
-									</li>
-									<li>
-										<a href="#home">Instagram</a>
-									</li>
-									<li>
-										<a href="#home">Twitter</a>
-									</li>
-									<li>
-										<a href="#home">YouTube</a>
-									</li>
-									<li>
-										<a href="#home">LinkedIn</a>
-									</li>
-									<li>
-										<a href="https://stories.strava.com">Stories</a>
-									</li>
-								</ul>
-							</div>
-							<div className="col-md-2">
-								<h5>Help</h5>
-								<ul className="list-unstyled">
-									<li>
-										<a href="https://strava.zendesk.com/home">Strava Support</a>
-									</li>
-								</ul>
-							</div>
-							<div className="col-md-2">
-								<h5>More</h5>
-								<ul className="list-unstyled">
-									<li>
-										<a href="/careers">Careers</a>
-									</li>
-									<li>
-										<a href="https://press.strava.com">Press</a>
-									</li>
-									<li>
-										<a href="https://business.strava.com?utm_source=footer&amp;utm_medium=referral">
-											Business
-										</a>
-									</li>
-									<li>
-										<a href="http://labs.strava.com/developers">Developers</a>
-									</li>
-									<li>
-										<a href="http://labs.strava.com">Labs</a>
-									</li>
-									<li>
-										<a href="/community-standards">
-											Strava Community standards
-										</a>
+										<a href="/getItOnGooglePlayLogo"><img src={getItOnGooglePlayLogo} alt="Google Play Logo" height={35}/></a>
 									</li>
 								</ul>
 							</div>
