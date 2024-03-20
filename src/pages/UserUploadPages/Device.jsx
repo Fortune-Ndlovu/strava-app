080bd9a159d09a896cc25868c38f8bed7b4f44c8
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-20 00:58:31 +0000

    Commenting our code not in use

--- src/pages/UserUploadPages/Device.jsx
+++ src/pages/UserUploadPages/Device.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import UploadActivitiesSideBar from '../../components/UploadActivitiesSideBar/UploadActivitiesSideBar';
+// import UploadActivitiesSideBar from '../../components/UploadActivitiesSideBar/UploadActivitiesSideBar';
 import '../../components/UploadActivitiesSideBar/UploadActivitiesSideBar.css';
 
 const Device = () => {
   return (
     <div  className="user-upload-container">
-        <UploadActivitiesSideBar/>
+        {/* <UploadActivitiesSideBar/> */}
         <h1>Device</h1> 
     </div>
   )
