3233c4eaccdcb426009a8c85d3580048a15ece2b
Fortune Ndlovu <ndlovufortune97@gmail.com>
2024-03-20 01:28:03 +0000

    Commenting out posts for temporarily.

--- src/pages/UserUploadPages/CreatePost.jsx
+++ src/pages/UserUploadPages/CreatePost.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import '../../components/CreateNewPostForm/CreateNewPostForm.css';
-import UserPostsManger from '../../services/UserPostsManager';
+// import UserPostsManger from '../../services/UserPostsManager';
 
 const CreatePost = () => {
   return (
     <div id="createPostContainer">
-      <UserPostsManger />
+      {/* <UserPostsManger /> */}
+      <h1>Post</h1>
     </div>
   )
 }
